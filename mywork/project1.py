299b54540f7210cee1f6cfab1b6d4441495d723c
joseph.guidry <joseph.guidry4@gmail.com>
2017-04-28 15:46:12 -0400

    Add project files

--- mywork/project1.py
+++ mywork/project1.py
@@ -9,8 +9,10 @@ import sys
 
 def get_bottles(args_list, num):
     """Gets list of command line arguments. Returns number of bottles"""
+     
     while len(args_list) > 1:        # If there is a command line arguement
-        args_list.pop(0)             # Remove filename from argument in the list.
+        args_list.pop(0)
+        #if int(args_list[0]) is type(1): # Remove non-integers from arguments in the list.
         return int(args_list[0])
     else:                            # Default to 99 if no arguments given.
         return (num)
@@ -24,7 +26,7 @@ def song(bottles):
     for bottle in range(bottles, 0, -1):
         print()
         print(bottle, beer, wall)              #Prints "# bottles of beer on the wall!
-        print(bottle, beer, \s"!")               #Prints "# bottles of beer!
+        print(bottle, beer + "!")              #Prints "# bottles of beer!
         print(take)
         bottle -= 1
         if bottle == 1:
@@ -32,14 +34,14 @@ def song(bottles):
         if bottle == 0: 
            beer = "No more bottles of beer"    # Change wording for no beers.
            print(beer, wall)
-           return                              #Returns to "No more bottles of beer
-                                               # on the wall"
+           return                              #Returns to "No more bottles of beer on the wall"
         print(bottle, beer, wall)              #Prints "(# - 1) bottles of beer on the wall!
         
 def main():
     """This is the programs main function"""
     
     bottle = get_bottles(sys.argv, num=99)
-    song(bottle)                               # Begin song "99 bottles of beer on the wall".  
+    song(bottle)                            # Begin song "99 bottles of beer on the wall". 
+    print() 
         
 if __name__=="__main__":main()
