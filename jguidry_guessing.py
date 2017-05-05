0c0352e1b53c741eaad0197eaf40d2da686f73da
joseph.guidry <joseph.guidry4@gmail.com>
2017-05-05 11:48:38 -0400

    Update guessing game

--- jguidry_guessing.py
+++ jguidry_guessing.py
@@ -12,23 +12,27 @@ import os
 
 def guess_game():
     """This is the main function to start the guessing game"""
-    banner_fmt = "{:^78}"
-    statement1 = "number between 1 and 100"
-    line_break = ("-" * 79)
+    print_banner()
     while True:
-        os.system('clear')
-        print(line_break + banner_fmt .format("The Guessing Game\n"))
-        print(banner_fmt.format("Guess the " + statement1 + "\n"))
-        print(line_break)
-        rand_num = game_numbers(100)
-        lie_num = game_numbers(10)
+        rand_num = game_numbers(100)        # Generate random number to guess
+        lie_num = game_numbers(10)          # Generate random number for lie
         end_turn = play_game(rand_num, lie_num)  # Start guessing game
         print("Finishing on turn: " + str(end_turn))
-        if lie_num < end_turn:                   # If answer after lie
+        if lie_num < end_turn:              # If correct guess was after lie
             print("I lied about the results on turn " + str(lie_num))
-        cont = input("Enter 'y' to play again:  ")
+        cont = input("Enter 'y' to play again: ")
         if cont != 'y':
-            break
+            break   # Exits game
+
+
+def print_banner():
+    """Print Banner of game and instructions"""
+    banner_fmt = "{:^78}"
+    line_break = ("-" * 79)
+    os.system('clear')  # Clear screen before printing banner.
+    print(line_break + banner_fmt .format("The High/Low Guessing Game\n"))
+    print(banner_fmt.format("Guess a number between 1 and 100\n"))
+    print(line_break)
 
 
 def play_game(rand_num, lie_num):
@@ -42,27 +46,27 @@ def play_game(rand_num, lie_num):
             if guess is rand_num:        # First guess evaluation
                 print("\nCorrect\n")
                 return turn
-            if guess < rand_num:         # Guess is lower evaluation
-                if turn is lie_num:      # Check if turn to lie
-                    lower(response_fmt)  # Print lie
+            if guess < rand_num:           # Guess is lower evaluation.
+                if turn is lie_num:        # Check if turn to lie.
+                    lower(response_fmt)    # Print lie.
                 else:
-                    higher(response_fmt)
-            else:                         # Higher guess response
-                if turn is lie_num:       # Check if turn to lie
-                    higher(response_fmt)
+                    higher(response_fmt)   # Print truth.
+            else:                          # Higher guess response.
+                if turn is lie_num:        # Check if turn to lie.
+                    higher(response_fmt)   # Print lie.
                 else:
-                    lower(response_fmt)
+                    lower(response_fmt)    # Print truth.
         except (ValueError, UnboundLocalError):
-            print("Enter a number between 1 and 100")
+            print("Enter a number between 1 and 100")          
 
 
 def higher(response_fmt):
-    """Outputs a formated response to guess higher"""
+    """Prints a formatted response to guess higher"""
     print(response_fmt.format("low", "bigger"))
 
 
 def lower(response_fmt):
-    """Returns a formated response to guess lower"""
+    """Prints a formatted response to guess lower"""
     print(response_fmt.format("high", "smaller"))
 
 
@@ -74,10 +78,10 @@ def game_numbers(upper_limit):
 def get_guess():
     """Return a integer value of valid guess"""
     guess = input("Enter your guess> ")
-    if not guess.isnumeric():           # Check if uess is number
+    if not guess.isnumeric():           # Check if guess is a number.
         raise UnboundLocalError
-    guess = int(guess)                  # Convert to integer
-    if guess > 100 | guess < 0:         # Check if out of range
+    guess = int(guess)                  # Convert to integer.
+    if guess > 100 | guess < 0:         # Check if out of range.
         raise ValueError
 
     return guess
